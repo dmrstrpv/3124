7c933377880fe63481d39ba08a658cb8ab5436b1
dmrstrpv <dmrstrpv@gmail.com>
2022-09-02 23:58:02 +0300

    hw_seminar2

---
+++ hw_seminar2/task13/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Random random_num = new Random();
+int num = random_num.Next(1, 10000000);
+Console.WriteLine ($"Сгенерированно случайное число {num}");
+
+string num_text = Convert.ToString(num);
+
+if (num_text.Length > 2)
+{
+    Console.WriteLine ($"{num} -> {num_text[2]}");
+}
+else 
+{
+    Console.WriteLine ($" {num} -> третьей цифры нет");
+}
---
+++ hw_seminar2/task15/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+Console.WriteLine ("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (5 < num && num < 8)
+{
+Console.Write(" -> Да");
+}
+
+else if (0 < num && num < 6)
+
+{
+  Console.Write(" -> Нет");  
+}
+
+else if (num < 0 || num > 7)
+
+{
+    Console.WriteLine("В неделе 7 дней"); 
+}
