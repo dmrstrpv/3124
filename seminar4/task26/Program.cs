7140daabbef6628cd71a49f6bf8c06c17dc2184b
dmrstrpv <dmrstrpv@gmail.com>
2022-09-18 20:44:36 +0300

    seminar4

---
+++ seminar4/task26/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetCountOfDigits(int num)
+{
+    int count = 0; //число цифр (количество итераций цикла)
+    if (num == 0) count = 1;
+    while(num != 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+
+int number = ReadNumber("Введите число:");
+int count = GetCountOfDigits(number);
+Console.WriteLine(count);
---
+++ seminar4/task28/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int ProdNumbersFrom1ToA(int a)
+{
+    if (a >= 1) //если А больше либо равно 1, то работаем дальше
+    {
+        int sum = 1;
+        for (int i = 2; i <= a; i++)
+        {
+            sum = sum * i; //копим произведение
+            //sum += i; //альтернативный вариант записи команды выше
+        }
+        return sum; //вернем накопившуюся сумму
+    }
+    else
+    {
+        Console.WriteLine("A должна быть не меньше 1!");
+        return -1; //неправильная сумма (код ошибки)
+    }
+}
+
+int number = ReadNumber("Введите число :");
+int sum = ProdNumbersFrom1ToA(number);
+Console.WriteLine($"Произведение элементов от 1 до A = {sum}");
