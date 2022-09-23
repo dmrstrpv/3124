a056fc307bf470ceb8f7da9991154356912f011c
dmrstrpv <dmrstrpv@gmail.com>
2022-09-23 18:38:17 +0300

    seminar4

---
+++ seminar4/test/Program.cs
@@ -0,0 +1,12 @@
+﻿int M = int.Parse(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < M; i++ )
+{
+    Console.WriteLine("Ввведите число ");
+    int n = int.Parse(Console.ReadLine());
+    if ( n > 0)
+    {
+        count ++;
+    }
+}
+Console.WriteLine($"{count} раз пользователь ввел числа больше нуля");
