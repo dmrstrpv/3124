573b5e935c45f686dd49fb09cd30461af5a3bce1
dmrstrpv <dmrstrpv@gmail.com>
2022-09-27 22:16:15 +0300

    hw_seminar5

---
+++ hw_seminar5/task36/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+int [] array = new int [4];
+
+void FillArray(int [] arr)
+{
+    for(int i = 0; i < arr.Length; i++) 
+    {
+        arr[i] = new Random().Next(1, 10);
+    }
+
+}
+
+int EvenPositionSum(int [] arr)
+{
+    int sum = 0;
+    for(int i = 1; i < arr.Length; i++) 
+    {
+        if (i % 2 != 0)
+        {
+            sum = sum + arr[i];
+        }
+        
+    }
+    return sum;
+
+}
+
+
+void PrintArray(int [] arr)
+{
+    for(int i = 0; i < arr.Length; i++) 
+    {
+        Console.Write($"{arr[i]} ");
+    }
+
+}
+
+FillArray(array);
+Console.WriteLine("Задан одномерный массив, заполненный случайными числами");
+PrintArray(array);
+EvenPositionSum(array);
+Console.WriteLine();
+Console.Write($"Сумма элементов, стоящих на нечётных позициях {EvenPositionSum(array)}");
---
+++ hw_seminar5/task38/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 38: Задайте массив натуральных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+
+int [] array = new int [5];
+
+void FillArray(int [] arr)
+{
+    for(int i = 0; i < arr.Length; i++) 
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+
+}
+
+int MaxMinArrayElementDiff(int [] arr)
+{
+    int diff = 0;
+    int max = 0;
+    int min = 10;
+    for(int i = 0; i < arr.Length; i++) 
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+        
+        diff = max - min;
+    }
+    return diff;
+
+}
+
+
+void PrintArray(int [] arr)
+{
+    for(int i = 0; i < arr.Length; i++) 
+    {
+        Console.Write($"{arr[i]} ");
+    }
+
+}
+
+FillArray(array);
+Console.WriteLine("Задан одномерный массив натуральных чисел.");
+PrintArray(array);
+MaxMinArrayElementDiff(array);
+Console.WriteLine();
+Console.Write($"Разница между максимальным и минимальным элементами массива {MaxMinArrayElementDiff(array)}");
