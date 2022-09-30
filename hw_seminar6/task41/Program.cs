232d9c4baa8be2b1bc02a029fdf5dfd2a858094d
dmrstrpv <dmrstrpv@gmail.com>
2022-09-30 18:26:41 +0300

    hw_seminar6

--- hw_seminar6/task41/Program.cs
+++ hw_seminar6/task41/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. Ввод чисел останавливается при помощи ввода стоп-слова "stop"
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+int CheckGreaterThanZero(int result)
+{
+    int greaterThanZeroNum = 0;
+    while (true)
+    {
+        string answer = Console.ReadLine();
+        if (answer == "stop")
+        {
+            break;
+        }
+        else
+        {
+            int.TryParse(answer, out int answerNumber);
+            if (answerNumber > 0)
+            {
+                greaterThanZeroNum++;
+            }
+        }
+    }
+    return greaterThanZeroNum;
+}
+
+
+int positiveNumCount = 0;
+Console.WriteLine("Введите число");
+Console.WriteLine($"Пользователь ввёл {CheckGreaterThanZero(positiveNumCount)} чисел больше 0");
+
