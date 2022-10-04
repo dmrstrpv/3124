78d99aad1ae357e9e46bd85a15d937550fcc5761
dmrstrpv <dmrstrpv@gmail.com>
2022-10-05 00:34:32 +0300

    hw_sminar9

---
+++ hw_seminar9/task64/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int ReadLine(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void WriteAllNumbers(int n)
+{
+    if (n <= 0)
+    {
+        return;
+    }
+    else
+    {
+        WriteAllNumbers(n - 1);
+        Console.Write(n + " ");
+    }
+}
+
+
+int N = ReadLine("Введите число: ");
+WriteAllNumbers(N);
---
+++ hw_seminar9/task66/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int ReadLine(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int NumSum(int m, int n)
+{
+    if (n == m)
+    {
+        return n;
+    }
+
+    else if (n > m)
+    {
+        return n + NumSum(m, n - 1);
+    }
+    
+    else
+        return n + NumSum(m, n + 1);
+
+}
+
+
+int m = ReadLine("Введите первое число:");
+int n = ReadLine("Введите второе число:");
+Console.WriteLine($"Сумма натуральных элементов -> {NumSum(m, n)}.");
