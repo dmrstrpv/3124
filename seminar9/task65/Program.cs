0a34abd86c2b52947845abbbf8b1ff3fffbda647
dmrstrpv <dmrstrpv@gmail.com>
2022-10-05 00:38:23 +0300

    seminar9

---
+++ seminar9/task65/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintMtoN(int m, int n)
+{
+    if (n < m)
+    {
+        return;
+    }
+    else
+    {
+        PrintMtoN(m, n - 1);
+        Console.Write(n + " ");
+    }
+}
+
+int number = ReadNumber("Введите первое число: ");
+int number1 = ReadNumber("Введите второе число: ");
+PrintMtoN(number, number1);
---
+++ seminar9/task67/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int NumSum(int n)
+{
+    if (n == 0) { return n; }
+    else { return NumSum(n / 10) + n % 10; }
+}
+
+int n = ReadNumber("Введите число:");
+Console.WriteLine(NumSum(n));
